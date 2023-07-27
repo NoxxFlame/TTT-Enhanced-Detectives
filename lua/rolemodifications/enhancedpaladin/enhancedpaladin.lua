0d12426c96529f091a72929c5b61228a7e2ee39c
Malivil <mlebson@outlook.com>
2023-07-27 09:36:50 -0400

    Updated to use replicated convars with global fallback

--- lua/rolemodifications/enhancedpaladin/enhancedpaladin.lua
+++ lua/rolemodifications/enhancedpaladin/enhancedpaladin.lua
@@ -10,14 +10,20 @@ local GetAllPlayers = player.GetAll
 
 util.AddNetworkString("EnhancedPaladin_ShowDamageAura")
 
--- ConVars
-local paladin_explosion_immune = CreateConVar("ttt_paladin_explosion_immune", "1")
-local paladin_explosion_protect_self = CreateConVar("ttt_paladin_explosion_protect_self", "1")
+local function GetReplicatedValue(onreplicated, onglobal)
+    if CRVersion("1.9.3") then
+        return onreplicated()
+    end
+    return onglobal()
+end
 
--- Globals
-hook.Add("TTTSyncGlobals", "EnhancedPaladin_TTTSyncGlobals", function()
-    SetGlobalBool("ttt_paladin_explosion_protect_self", paladin_explosion_protect_self:GetBool())
-end)
+-------------
+-- CONVARS --
+-------------
+
+local paladin_explosion_immune = CreateConVar("ttt_paladin_explosion_immune", "1")
+
+local paladin_explosion_protect_self = GetConVar("ttt_paladin_explosion_protect_self")
 
 -- Explosion damage reduction
 hook.Add("EntityTakeDamage", "EnhancedPaladin_EntityTakeDamage", function(ent, dmginfo)
@@ -26,7 +32,12 @@ hook.Add("EntityTakeDamage", "EnhancedPaladin_EntityTakeDamage", function(ent, d
     if GetRoundState() >= ROUND_ACTIVE and ent:IsPlayer() and paladin_explosion_immune:GetBool() and dmginfo:IsExplosionDamage() then
         if not ent:IsPaladin() or paladin_explosion_protect_self:GetBool() then
             local paladin = nil
-            local radius = GetGlobalFloat("ttt_paladin_aura_radius", 262.45)
+            local radius = GetReplicatedValue(function()
+                    return GetConVar("ttt_paladin_aura_radius"):GetInt() * UNITS_PER_METER
+                end,
+                function()
+                    return GetGlobalFloat("ttt_paladin_aura_radius", 262.45)
+                end)
             for _, v in pairs(GetAllPlayers()) do
                 if v:IsActivePaladin() and v:GetPos():Distance(ent:GetPos()) <= radius then
                     paladin = v
--- lua/rolemodifications/enhancedpaladin/shared.lua
+++ lua/rolemodifications/enhancedpaladin/shared.lua
@@ -4,6 +4,8 @@ AddCSLuaFile()
 -- ROLE CONVARS --
 ------------------
 
+CreateConVar("ttt_paladin_explosion_protect_self", "1", FCVAR_REPLICATED)
+
 table.insert(ROLE_CONVARS[ROLE_PALADIN], {
     cvar = "ttt_paladin_explosion_immune",
     type = ROLE_CONVAR_TYPE_BOOL
